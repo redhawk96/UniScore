f37c4afe16207d5ed738d4f64c6f655d4fd698c9
Subarshan Thiyagarajah <tsubarshan@gmail.com>
2020-04-04 23:01:01 +0530

    Added all the Navigation panels and changed the icons.
    Signed-off-by: Subarshan Thiyagarajah <tsubarshan@gmail.com>

---
+++ UniScore-Server/src/admin/panels/content/LecturerContentPanel.java
@@ -0,0 +1,61 @@
+package admin.panels.content;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import com.panels.ContentPanel;
+import com.utils.ContentTable;
+import com.utils.UI;
+
+public class LecturerContentPanel extends ContentPanel {
+	JPanel contentPanel = new JPanel();
+	ContentTable table = new ContentTable();
+	JScrollPane scrollPane = new JScrollPane();
+
+	public LecturerContentPanel() {
+		contentPanel.setName("lecturer");
+		contentPanel.setBounds(UI.CONTENT_PANEL_X_AXIS, UI.CONTENT_PANEL_Y_AXIS, UI.CONTENT_PANEL_WIDTH, UI.CONTENT_PANEL_HEIGHT);
+		contentPanel.setBackground(UI.CONTENT_PANEL_BACKGROUND_COLOR);
+		contentPanel.setLayout(null);
+		
+		try {
+			
+			displayNavigationIndicator();
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	/*
+	 * returns the JPanel inside ContentPanel
+	 * @param {}
+	 * @returns JPanel
+	 */
+	public JPanel getContent() {
+		return contentPanel;
+	}
+	
+	
+	public void displayNavigationIndicator() {
+		JPanel navigationIndicatorPanel = new JPanel();
+		navigationIndicatorPanel.setBorder(UI.NAVIGATION_INDICATOR_PANEL_BORDER);
+		navigationIndicatorPanel.setBackground(UI.NAVIGATION_INDICATOR_PANEL_BACKGRIOUND_COLOR);
+		navigationIndicatorPanel.setBounds(30, 11, 1199, 36);
+		contentPanel.add(navigationIndicatorPanel);
+		navigationIndicatorPanel.setLayout(null);
+		
+		JLabel navigationIndicatorMainLabel = new JLabel("Lecturer /");
+		navigationIndicatorMainLabel.setBounds(1074, UI.NAVIGATION_INDICATOR_PANEL_Y_AXIS, 71, UI.NAVIGATION_INDICATOR_PANEL_HEIGHT);
+		navigationIndicatorMainLabel.setFont(UI.NAVIGATION_INDICATOR_PANEL_FONT);
+		navigationIndicatorMainLabel.setForeground(UI.NAVIGATION_INDICATOR_PANEL_MAIN_TEXT_COLOR);
+		navigationIndicatorPanel.add(navigationIndicatorMainLabel);
+		
+		JLabel navigationIndicatorActiveLabel = new JLabel("Lecturer");
+		navigationIndicatorActiveLabel.setFont(UI.NAVIGATION_INDICATOR_PANEL_FONT);
+		navigationIndicatorActiveLabel.setBounds(1139, UI.NAVIGATION_INDICATOR_PANEL_Y_AXIS, 50, UI.NAVIGATION_INDICATOR_PANEL_HEIGHT);
+		navigationIndicatorActiveLabel.setForeground(UI.NAVIGATION_INDICATOR_PANEL_ACTIVE_TEXT_COLOR);
+		navigationIndicatorPanel.add(navigationIndicatorActiveLabel);
+	}
+}
