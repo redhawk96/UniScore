a9ef774b90de07c470162a7f2f439bb91669a6db
redhawk96 <uditha1003@gmail.com>
2020-04-06 21:44:46 +0530

    User model updated

--- UniScore-Client/src/models/User.java
+++ UniScore-Client/src/models/User.java
@@ -1,3 +1,12 @@
+/*
+ * Institute	: SLIIT
+ * Module		: Comparative Integrated Systems
+ * Project Name	: UniScore
+ * Project		: Online Examination Management System
+ * Group		: 19
+ * Author		: Subarshan Thiyagarajah (UOB-1939088)
+ */
+
 package models;
 
 import java.io.Serializable;
@@ -6,20 +15,19 @@ import java.sql.Timestamp;
 @SuppressWarnings("serial")
 public class User implements Serializable{
 
-	private String userId, firstName, lastName, gender, email, nic, address, avatar, role, password, status;
+	private String userId, firstName, lastName, gender, email, nic, address, role, password, status;
 	private int phone;
 	private Timestamp registeredDate;
 	
 	public User() {}
 	
-	public User(String firstName, String lastName, String gender, String email, String nic, String address, String avatar, String role, Timestamp registeredDate, String password, String status, int phone) {
+	public User(String firstName, String lastName, String gender, String email, String nic, String address, String role, Timestamp registeredDate, String password, String status, int phone) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.gender = gender;
 		this.email = email;
 		this.nic = nic;
 		this.address = address;
-		this.avatar = avatar;
 		this.role = role;
 		this.registeredDate = registeredDate;
 		this.password = password;
@@ -27,7 +35,7 @@ public class User implements Serializable{
 		this.phone = phone;
 	}
 
-	public User(String userId, String firstName, String lastName, String gender, String email, String nic, String address, String avatar, String role, Timestamp registeredDate, String password, String status, int phone) {
+	public User(String userId, String firstName, String lastName, String gender, String email, String nic, String address, String role, Timestamp registeredDate, String password, String status, int phone) {
 		this.userId = userId;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -35,7 +43,6 @@ public class User implements Serializable{
 		this.email = email;
 		this.nic = nic;
 		this.address = address;
-		this.avatar = avatar;
 		this.role = role;
 		this.registeredDate = registeredDate;
 		this.password = password;
@@ -97,14 +104,6 @@ public class User implements Serializable{
 
 	public void setAddress(String address) {
 		this.address = address;
-	}
-
-	public String getAvatar() {
-		return avatar;
-	}
-
-	public void setAvatar(String avatar) {
-		this.avatar = avatar;
 	}
 
 	public String getRole() {
